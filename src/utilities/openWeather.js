f47eb2a173d3105764342716e944a397ef9bc6ca
Mara's Mac <williamsmc@my.lanecc.edu>
2023-09-21 15:16:40 -0700

    build location query string

--- src/utilities/openWeather.js
+++ src/utilities/openWeather.js
@@ -5,17 +5,23 @@ export default class OpenWeather {
         //this.scheme ='https';
         //this.subdomain ='api';
         //this.domain ='openweathermap.org';
-        this.apiVersion='2.5';
         //this.path='data/'+ this.apiVersion + '/forecast';
         //this.queryString='';
         //this.apiKey = 'appid=' + process.env.REACT_APP_WEATHER_KEY;
+        this.apiVersion='2.5';
+
         this.keyOnly = process.env.REACT_APP_WEATHER_KEY.substring(6);
        
-        this.queryParameters = {
+        this.weatherQueryData = {
             units: 'imperial',
             lat : '44.0682',
             lon :'-123.0819',
             appid : this.keyOnly
+        };
+
+        this.locationQueryData = {
+            postCode : '',
+            country : ''
         };
        
 
@@ -23,33 +29,68 @@ export default class OpenWeather {
             scheme : 'https://',
             domain : 'api.openweathermap.org/',
             path: 'data/'+ this.apiVersion + '/forecast',
-            queryString : '?' + this.buildQueryString()
+            queryString : ''
         }
         //methods that run on init
-        //this.buildQueryString();
+        
         this.buildWeatherUrl();
-        //"https://api.openweathermap.org/data/2.5/onecall?"
+
         //binding
-        //const weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?
-        //units=imperial&lat='+lat+'&lon='+lng+'&';
+      
 
     }
 
     //takes a js object with key value pairs, iterates over it
     //reduce array method
-    buildQueryString(){
-        let queryString ='';
-        for (let key in this.queryParameters){
-            queryString+= encodeURIComponent(key) + '='
-            + encodeURIComponent(this.queryParameters[key]) + '&';
+
+    setLocationValues (postCode, country) {
+        this.locationQueryData.postCode=postCode;
+        this.locationQueryData.country=country;
+    }
+
+    setWeatherQueryValues
+
+    setQueryParams(type){
+        
+        //weather query 
+        const weatherString = () => {
+            let queryString ='';
+            for (let key in this.weatherQueryParams){
+                queryString+= encodeURIComponent(key) + '='
+                + encodeURIComponent(this.weatherQueryParams[key]) + '&';
+            }
+             //take off the final '&'
+            queryString = queryString.slice(0, queryString.length-1);
+            return queryString;
         }
+        //location query uses zip={value},US&appid={value}
+
+        const locationString = () =
+        const paramObj =(type) => {
+            return type=='weather'? this.weatherQueryParams : this.locationQueryParams;
+        };
+
+        //check the value of the paramObj
+        console.log('query object is set to: '+paramObj);
+
+        //turning key value pairs into string
+        for (let key in paramObj){
+            queryString+= encodeURIComponent(key) + '='
+            + encodeURIComponent(paramObj[key]) + '&';
+        }
+
         //take off the final '&'
         queryString = queryString.slice(0, queryString.length-1);
+
+        //messy way to handle the location data
+        if(type=='zip')
+            queryString =+ ',US'
         return queryString;
     }
 
-
     buildWeatherUrl () {
+        //set the value of queryString by passing in type
+        this.urlComponents.queryString = this.setQueryParams('weather');
         let values = Object.values(this.urlComponents);
         //let weatherUrl = values.reduce (whole, part) => {
         let weatherUrl = values.reduce((whole, part) =>{
