1487d2a67103216d17637a521fdf28080643177f
Mara's Mac <williamsmc@my.lanecc.edu>
2023-09-25 19:01:48 -0700

    move to new branch

--- src/components/Weatherlist.js
+++ src/components/Weatherlist.js
@@ -2,18 +2,13 @@ import WeatherListItem from "./WeatherListitem";
 import '../styles/WeatherList.css';
 
 function WeatherList ({onDayClick, forecast}) {
-    //create array of react components
-    //mapping function to transform the forecast array into a new array of WeatherListItems
-
     const renderedWeatherItems = forecast.map ((item) => {
         return <WeatherListItem key = {item.dt} forecastDay={item} index = {forecast.indexOf(item)} onDayClick={onDayClick} />
     })
     
-    //return the rendered components in single div
     return (
         <div className="weather-list">{renderedWeatherItems}</div>
     );
-
 }
 
 export default WeatherList;
