f1e7e411dba208804a26d788e18c5b459c095a44
Mara's Mac <williamsmc@my.lanecc.edu>
2023-09-28 15:45:25 -0700

    select box correctly setting units onSelect

--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -2,10 +2,9 @@ import {useState} from 'react';
 
 function ZipForm ({onSubmit}){
     const [zip, setZip] = useState('');
-    const [units, setUnits] = useState('');
-    const [formUnits, setFormUnits] =useState('');
     const errorMsgDiv = document.getElementById("openWeather-error");
-
+    const [units, setUnits] = useState('imperial');
+    
     //event handler that sends the zip code value back to App.js
     const handleFormSubmit = (event) => {
         event.preventDefault();
@@ -19,7 +18,7 @@ function ZipForm ({onSubmit}){
         //if zip is valid call onSubmit
         //if zip is not valid create a child element on screen (not a react component)
         if(pattern5DigitZip.test(zip)){
-            onSubmit(zip, formUnits);
+            onSubmit(zip, units);
         }
         else
         {
@@ -36,37 +35,23 @@ function ZipForm ({onSubmit}){
 
     const handleUnitsChange = (event) => {
         const test = event.target.value;
-        setFormUnits(test);
-        alert({formUnits}+'is units and ' + {test} + "is select value");
+        setUnits(test);
     };
-    const handleCheckChange = (event) => { 
-        setFormUnits(event.target.value);
-        console.log('The checkbox was toggled: '+ formUnits);       
-      }; 
+
     return (
         <div className="zip-form col-md-6">
-            <form id="zipForm" className="d-flex flex-row" onSubmit={handleFormSubmit}>
+            <form id="zipForm" className="d-flex flex-row" onSubmit={handleFormSubmit} >
                 <div className="m-3">
                 <div>
-                    <div className='input-group'>
-                        <input type="radio" name="units-radio" value="imperial" id="imperial" 
-                            checked ={formUnits ==="imperial"}
-                            onChange={handleCheckChange}/>
-                        <label htmlFor="imperial">Imperial</label>
-                    </div>
-                    <div className='input-group'>
-                        <input type="radio" name="units-radio" value="metric" id="metric" 
-                            checked ={formUnits ==="metric"}
-                            onChange={handleCheckChange}/>
-                        <label htmlFor="metric">Metric</label>
-                    </div>
-                    <div>
-                        <input type="radio" name="units-radio" value="null" id="null" 
-                            checked ={formUnits ==="null"}
-                            onChange={handleCheckChange}/>
-                        <label htmlFor="null">None</label>
-                    </div>
-                </div> 
+                    <select
+                    value={units} // ...force the select's value to match the state variable...
+                    onChange={e => setUnits(e.target.value)} // ... and update the state variable on any change!
+                    >
+                        <option value="imperial">Imperial</option>
+                        <option value="metric">Metric</option>
+                        <option value="standard">Standard</option>
+                     </select>
+                </div>
                     <div>
                         <label className="my-3">Enter a five digit Zipcode:</label>
                         <input 
@@ -76,14 +61,7 @@ function ZipForm ({onSubmit}){
                         />
                     </div>
                     <div>
-                    <select className="form-select"
-                        value={formUnits}
-                        onSelect={handleUnitsChange}
-                        >
-                        <option value="metric">Metric</option>
-                        <option value="imperial">Imperial</option>
-                        <option value="none">None</option>
-                    </select> 
+
                     </div>
                     <button type="submit" className="my-3 btn btn-success"> Get the forecast!</button>
                 </div>
